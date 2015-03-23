99a0620a8d5a44ab744d38ab7885140285793fa3
drice0331 <d.rice0331@gmail.com>
2015-03-22 20:40:29 -0700

    added webview for fb and twitter, work on basetask and parser

--- iPitSlo/src/com/thepit/ipitslo/ui/BlogActivity.java
+++ iPitSlo/src/com/thepit/ipitslo/ui/BlogActivity.java
@@ -1,12 +1,16 @@
 package com.thepit.ipitslo.ui;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.thepit.ipitslo.R;
 import com.thepit.ipitslo.adapter.BlogAdapter;
 import com.thepit.ipitslo.model.BlogEntry;
-import com.thepit.ipitslo.util.BlogAsyncTask;
+import com.thepit.ipitslo.util.BaseFetchTask;
+import com.thepit.ipitslo.util.BaseFetchTask.FetchTaskListener;
+import com.thepit.ipitslo.util.CoreConstants;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,18 +19,28 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
-public class BlogActivity extends BaseActivity {
+public class BlogActivity extends BaseActivity implements FetchTaskListener<BlogEntry>{
 	
 	private ListView blogListView;
 	private BlogAdapter adapter;
 	private ArrayList<BlogEntry> mBlogEntries;
+	private Map<String, String> parserKeys;
+	private Map<String, String> objectKeys;
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_blog);
 		blogListView = (ListView)findViewById(R.id.blog_list_view);
 		
-		new BlogAsyncTask().execute();
+		parserKeys = new HashMap<String, String>();
+		parserKeys.put(CoreConstants.DATA_URL, "http://thepitslo.blogspot.com/feeds/posts/default?alt=rss");
+		parserKeys.put(CoreConstants.CLASS_NAME, "BlogEntry");
+		
+		objectKeys = new HashMap<String, String>();
+		objectKeys.put("field1", "title");
+		objectKeys.put("field2", "link");
+		
+		new BaseFetchTask<BlogEntry>(this).execute(parserKeys, objectKeys);
 
 		adapter = new BlogAdapter(this, android.R.layout.simple_list_item_1, mBlogEntries);
 		blogListView.setAdapter(adapter);
@@ -39,5 +53,10 @@ public class BlogActivity extends BaseActivity {
 		        startActivity(intent);				
 			}
 		});
+	}
+
+	@Override
+	public void onPostExecute(ArrayList<BlogEntry>itemList) {
+		mBlogEntries = itemList;
 	}
 	}
